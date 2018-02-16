bccb99ecb91a80819f36834fc32d510e22257987
Nicko Dela Cruz <nickodc96@gamil.com>
2018-02-16 10:53:21 -0800

    copy pasted code from my favorite-things assignment

--- lib/words.rb
+++ lib/words.rb
@@ -1,6 +1,39 @@
 #!/usr/bin/ruby
 
-class Word
+class Contact
   @@list = []
 
-  attr_accessor :word, :define
+  attr_accessor :first, :last, :title, :company
+
+  attr_reader :id
+
+  def initialize(attributes)
+    @first = attributes.fetch(:first)
+    @last = attributes.fetch(:last)
+    @title = attributes.fetch(:title)
+    @company = attributes.fetch(:company)
+    @id = @@list.length + 1
+  end
+
+  def self.all()
+    @@list
+  end
+
+  def self.clear()
+    @@list = []
+  end
+
+  def save()
+    @@list.push(self)
+  end
+
+  def self.find(id)
+    contact_id = id.to_i()
+    @@list.each do |contact|
+      if contact.id == contact_id
+        return contact
+      end
+    end
+  end
+
+end
